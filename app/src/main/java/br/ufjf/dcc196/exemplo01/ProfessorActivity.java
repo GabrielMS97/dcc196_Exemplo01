85e939ffd6b488b959f86eb4543cab9e9cae7f39
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-08-29 19:59:39 -0300

    Cria um intent para a ProfessorActivity

--- app/src/main/java/br/ufjf/dcc196/exemplo01/ProfessorActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo01/ProfessorActivity.java
@@ -3,7 +3,7 @@ package br.ufjf.dcc196.exemplo01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class ProfessorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
