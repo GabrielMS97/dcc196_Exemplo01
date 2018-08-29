0028a0b99ac55075ee50a6d1f4e2c06d9c7145f5
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-08-29 20:14:26 -0300

    Adiciona extras no intent para Professor

--- app/src/main/java/br/ufjf/dcc196/exemplo01/ProfessorActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo01/ProfessorActivity.java
@@ -2,12 +2,22 @@ package br.ufjf.dcc196.exemplo01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfessorActivity extends AppCompatActivity {
+
+    private TextView txtProfMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        txtProfMensagem = findViewById(R.id.txt_profMensagem);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            String nome = extras.getString("nome");
+            txtProfMensagem.setText("Olá professor " + nome);
+        }
     }
 }
