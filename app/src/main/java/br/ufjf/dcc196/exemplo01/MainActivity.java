f45d93be8c377c51127c40394210f402d804ba45
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-09-05 19:53:12 -0300

    Adicionando a comunicação entre as telas

--- app/src/main/java/br/ufjf/dcc196/exemplo01/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo01/MainActivity.java
@@ -1,24 +1,32 @@
 package br.ufjf.dcc196.exemplo01;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String PESSOA_NOME = "nome";
+    private static final int REQUEST_SIAPE = 1;
+    public static final String PROF_SIAPE = "siape";
+
     private Button btnProfessor;
     private EditText edtNome;
+    private TextView txtMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtNome = findViewById(R.id.edt_nome);
+        edtNome = (EditText) findViewById(R.id.edt_nome);
+
+        txtMensagem = (TextView)findViewById(R.id.txt_prontos);
 
         btnProfessor = findViewById(R.id.btn_professor);
         btnProfessor.setOnClickListener(new View.OnClickListener() {
@@ -26,8 +34,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ProfessorActivity.class);
                 intent.putExtra(PESSOA_NOME, edtNome.getText().toString());
-                startActivity(intent);
+                startActivityForResult(intent, MainActivity.REQUEST_SIAPE);
             }
         });;
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MainActivity.REQUEST_SIAPE && resultCode == Activity.RESULT_OK){
+
+        }
+        Bundle bundleRes = data.getExtras();
+        String siape = bundleRes.getString(MainActivity.PROF_SIAPE);
+        txtMensagem.setText("Olá professor de Siape " + siape);
     }
 }
